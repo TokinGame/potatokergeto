117b6f5d8da3c1fab087f80405b272ec63f932d6
Marcell Schuh <marcell.schuh@gmail.com>
2017-09-28 14:53:04 +0200

    Potato

---
+++ android/src/tech/tokin/game/potatokergeto/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package tech.tokin.game.potatokergeto;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tech.tokin.game.potatokergeto.UltimateGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UltimateGame(), config);
+	}
+}
